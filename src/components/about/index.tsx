e32e1697a91e7fbb70137175e935493f39fb9eb8
Krishna Kumar <dev@krishnaaa.com>
2024-08-10 22:23:10 +0530

    fixed about section z index

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -18,7 +18,7 @@ const About = () => {
       </div>
 
       <div className="w-full overflow-hidden from-[#8D9853] to-[#2E321B] rounded-2xl bg-gradient-to-br py-4 px-8 sm:py-8 sm:px-12 md:py-16 md:px-24 2xl:px-44 2xl:py-24 relative flex flex-col gap-8 sm:gap-12">
-        <div className="absolute -top-[14rem] -left-[14rem] scale-50 md:scale-100 md:-top-[14rem] md:-left-[14rem] select-none flex items-center justify-center">
+        <div className="z-[1] absolute -top-[14rem] -left-[14rem] scale-50 md:scale-100 md:-top-[14rem] md:-left-[14rem] select-none flex items-center justify-center">
           <Image
             className="object-cover h-fit w-full"
             src="/assets/vector-about.png"
@@ -28,7 +28,7 @@ const About = () => {
           />
         </div>
 
-        <div className="absolute -bottom-[14rem] -right-[12rem] scale-50 md:scale-100 md:-bottom-[14rem] md:-right-[10rem] select-none flex items-center justify-center">
+        <div className="z-[1] absolute -bottom-[14rem] -right-[12rem] scale-50 md:scale-100 md:-bottom-[14rem] md:-right-[10rem] select-none flex items-center justify-center">
           <Image
             className="object-cover h-fit w-full"
             src="/assets/vector-about.png"
@@ -38,11 +38,11 @@ const About = () => {
           />
         </div>
 
-        <span className="flex items-center gap-1 text-lg font-semibold">
+        <span className="z-10 flex items-center gap-1 text-lg font-semibold">
           <GoDotFill size={25} />
           Introduction
         </span>
-        <div className="!leading-relaxed text-2xl sm:text-3xl md:text-5xl font-semibold">
+        <div className="z-10 !leading-relaxed text-2xl sm:text-3xl md:text-5xl font-semibold">
           At the forefront of{" "}
           <span className="font-instrumentSerif italic">design</span>{" "}
           innovation, our agency harnesses digital{" "}
@@ -51,15 +51,17 @@ const About = () => {
           <span className="font-instrumentSerif italic">innovate</span> rapidly
           and effectively.
         </div>
-        <div className="flex w-fit gap-1 hover:flex-row-reverse hover:justify-end duration-300 ease-in-out transition-all">
+        <div className="z-10 flex w-fit gap-1 hover:flex-row-reverse hover:justify-end hover:duration-300 hover:ease-in-out hover:transition-all">
           <Link
-            href="#contact-us"
+            href="https://calendly.com/alotavocados/30min"
+            target="_blank"
             className="p-3 bg-white rounded-xl text-black font-semibold text-sm md:text-lg"
           >
             Book a 15-min call
           </Link>
           <Link
-            href="#contact-us"
+            href="https://calendly.com/alotavocados/30min"
+            target="_blank"
             className="p-3 px-4 bg-white rounded-xl text-black font-semibold text-lg flex items-center"
           >
             <IoArrowForwardOutline className="h-5 w-5" />
