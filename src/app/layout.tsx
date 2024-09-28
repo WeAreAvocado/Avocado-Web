785ddec803f8f594b6e6f6345767c245dc188591
Avocado Tech <156170437+WeAreAvocado@users.noreply.github.com>
2024-09-28 14:30:50 +0000

    viewport error fix 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ const jsonLd: WithContext<FAQPage> = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  minimumScale: 1,
   maximumScale: 1,
   userScalable: false,
 }
